0ae5d07ab0c8d745caff0dc51a7b301dad823cd5
Ana Riat <t.riat24@gmail.com>
2019-05-12 09:22:18 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,5 +8,5 @@ def batch_badge_creator(attendees)
   end
   def assign_rooms(attendees)
     attendees.each_with_index.map do |attendee, index|
-      "Hello, #{attendee}!"
+      "Hello, #{attendee}! You'll be assigned to room index+1"
       
