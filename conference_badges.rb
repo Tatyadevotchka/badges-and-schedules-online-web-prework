f43daa08bb3d63c1e0740e8886b45529bc91750a
Ana Riat <t.riat24@gmail.com>
2019-05-12 09:24:15 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,5 +12,7 @@ def batch_badge_creator(attendees)
     end
     def printer (attendees)
       batch_badge_creator(attendees).each do |badge|
-        
+        puts badge
+      end
+      
       
